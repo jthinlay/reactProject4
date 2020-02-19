4cd5a477a45f471b5010ac0cdbd01f20662b4bc2
Jampa Thinlay <jampathinlay@Jampas-MacBook-Pro.local>
2020-02-19 14:10:04 -0800

    completed notifications and real live update the project content and the notifications update, orderby desc and limited 3

--- src/components/dashborad/dashboard/dashboard.jsx
+++ src/components/dashborad/dashboard/dashboard.jsx
@@ -9,9 +9,9 @@ import {Redirect} from 'react-router-dom'
 
 class Dashboard extends React.Component{
     render(){
-        const {auth} = this.props
+        const {projects, auth, notifications} = this.props
         if(!auth.uid) return < Redirect to="/signin" />
-        const {projects} = this.props
+        
         return(
             <div className="dashboard">
                 <div className="row">
@@ -19,7 +19,7 @@ class Dashboard extends React.Component{
                         <ProjectList projects = {projects}/>
                     </div>
                     <div className="col2">
-                        <Notifications />
+                        <Notifications notifications = {notifications}/>
                      </div>
                 </div>
             </div>
@@ -27,15 +27,17 @@ class Dashboard extends React.Component{
     }
 }
 const mapStateToProps = ( state ) => {
-    console.log(state)
+    console.log('tsarong', state)
     return{
         projects: state.firestore.ordered.project,
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        notifications: state.firestore.ordered.notifications
     }
 }
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        {collection: 'project'}
+        {collection: 'project', orderBy: ['createdAt', 'desc']},
+        {collection: 'notifications', limit: 3, orderBy: ['time', 'desc']}
     ])
 )(Dashboard)
--- src/components/dashborad/notifications/notifications.jsx
+++ src/components/dashborad/notifications/notifications.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React from 'react';
 import './notifications.styles.scss';
+import moment from 'moment';
 
 
-const Notifications = () => {
+const Notifications = (props) => {
+    const {notifications} = props;
+    console.log('jt', notifications)
     return(
         <div className="section">
             <div className="card">
                 <div className="card-content">
                     <span className='card-title'>Notifications </span>
                     <ul className='notifications'>
-                        <li>Notification</li>
-                        <li>Notification</li>
+                        {notifications && notifications.map(item => {
+                            return (
+                                <li key={item.id}>
+                                    <span><i>{item.user} </i></span>
+                                    <span>{item.content} </span>
+                                    <div className="notification-time">{moment(item.time.toDate()).fromNow()}</div>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
             </div>
